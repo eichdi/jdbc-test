b29e15c6897f672a2c47129bd1b7e058aa1c2a94
eichdi <eichdi96@gmail.com>
2016-06-29 20:11:31 +0300

    add tests

--- src/test/java/UserServiceTest.java
+++ src/test/java/UserServiceTest.java
@@ -8,6 +8,9 @@ import ru.test.jdbc.user.dao.UsersDao;
 import ru.test.jdbc.user.model.User;
 import ru.test.jdbc.user.service.UserServiceImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.when;
 import static org.junit.Assert.*;
@@ -43,9 +46,18 @@ public class UserServiceTest {
         return new User("name","info","email",1,2000,true);
     }
 
+    public static List<User> userList(){
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("name","info","email",1,2000,true));
+        userList.add(new User("name1","info1","email1",2,2001,false));
+        return  userList;
+    }
+
     @Before
     public void setUp(){
         when(usersDao.findeUserById(0)).thenReturn(anyUser());
+        when(usersDao.findAll()).thenReturn(userList());
+        when(usersDao.findNameById(0)).thenReturn("name");
     }
 
     @Test
@@ -54,5 +66,23 @@ public class UserServiceTest {
         User expected = anyUser();
         UserEquals(expected, actual);
     }
+
+    @Test
+    public void testGetAllUsers(){
+        List<User> actual = service.getAllUsers();
+        List<User> expected = userList();
+        assertEquals(actual.size(),expected.size());
+        for (int i=0; i<actual.size(); i++){
+            UserEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testGetNameById(){
+        String actual = service.getNameById(0);
+        String expected = "user";
+        assertEquals(expected, actual);
+    }
+
 
 }
